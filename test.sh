d744d85277b08bff9135cb310be65f417efc4182
Jan Wagner <jenik.wagner@gmail.com>
2020-11-03 02:05:46 +0100

    -s -L

--- test.sh
+++ test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+#Change this to suit your source codes! OR USE -s <source_files>
+SOURCE_FILES="main.c" 
 
-TIMEOUT=0
+TIMEOUT=0 #for timeout... use with -t <TIMEOUT>
 RETURN=0
-REPEAT=1
-DO_DIFF=""
-IGNORE_TESTS=""
-IGNORE_REGEX=""
+REPEAT=1 #for repeated tests... use with -r <REPETITIONS>
+DO_DIFF="" # use -d if you want to see the diff
+IGNORE_TESTS="" #for see help for test ignoring
+IGNORE_REGEX="" #for see help for test ignoring
 TESTS_TO_IGNORE=()
 # Binarka:
-PROGRAM=./main.out
+PROGRAM="./main.out" 
 LANG="C"
-SOURCE_FILES="main.c utils.c utils.h"
+
+
 compile(){
 
     case $LANG in
@@ -35,6 +38,8 @@ show_help(){
 
     printf "Usage: ./hw02.sh ([OPTION] [ARGS]?)*\
     \n  -h,            prints help\
+    \n  -s,            specify source files (main.c is default)\
+    \n  -L,            choose language (just \"C\" for now)\
     \n  -d,            prints also difference your_out/datapub_out\
     \n  -i <TESTS>,    ignore certain tests, where <TESTS> are relative paths in datapub dir (<string>)\
     \n  -iR <TESTS_R>, ignore tests in datapub with regex <TESTS_R>\
@@ -76,22 +81,6 @@ rm -f program.out
 
 }
 
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -h) show_help; exit 0;;
-        -d) DO_DIFF="yes";;
-        -t) TIMEOUT="$2"; printf "Running with timeout = $TIMEOUT\n";shift;;
-        -r) REPEAT="$2"; printf "Running with repeat = $REPEAT\n";shift;;
-        -i) TESTS_TO_IGNORE+=("$2"); IGNORE_TESTS="yes"; printf "Ignoring... = '%s'\n" "${TESTS_TO_IGNORE[*]}";shift;;
-        -iR) IGNORE_REGEX="$2"; IGNORE_TESTS="yes"; printf "Ignoring... = '%s'\n" "$IGNORE_REGEX";shift;;
-        
-        *) show_help; exit 1 ;;
-    esac
-    shift
-done
-
-
 #if no args were passed just do plain run
 main(){
 compile
@@ -102,6 +91,22 @@ do
 done
 tput bel
 }
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -h) show_help; exit 0;;
+        -s) SOURCE_FILES="$2"; printf "Using %s source files...\n" "$SOURCE_FILES";shift;;
+        -L) LANG="$2";printf "Language %s...\n" "$LANG";shift;;
+        -d) DO_DIFF="yes";;
+        -t) TIMEOUT="$2"; printf "Running with timeout = $TIMEOUT\n";shift;;
+        -r) REPEAT="$2"; printf "Running with repeat = $REPEAT\n";shift;;
+        -i) TESTS_TO_IGNORE+=("$2"); IGNORE_TESTS="yes"; printf "Ignoring... = '%s'\n" "${TESTS_TO_IGNORE[*]}";shift;;
+        -iR) IGNORE_REGEX="$2"; IGNORE_TESTS="yes"; printf "Ignoring... = '%s'\n" "$IGNORE_REGEX";shift;;
+        
+        *) show_help; exit 1 ;;
+    esac
+    shift
+done
 
 main
 
