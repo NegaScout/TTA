45dfdd704ddb7454bd6fb8a36393cc5deef71353
Jan Wagner <jenik.wagner@gmail.com>
2020-11-16 02:46:29 +0100

    LANGs to Makefile

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#Change this to suit your source codes! OR USE -s <source_files>
-SOURCE_FILES="main.c" 
+#Change this to suit your needs! 
+#All the necessary tweaks should be available through switches
 
 TIMEOUT=0 #for timeout... use with -t <TIMEOUT>
 RETURN=0
 REPEAT=1 #for repeated tests... use with -r <REPETITIONS>
 DO_DIFF="" # use -d if you want to see the diff
-IGNORE_TESTS="" #for see help for test ignoring
-IGNORE_REGEX="" #for see help for test ignoring
+IGNORE_TESTS="" #see help for test ignoring
+IGNORE_REGEX="" #see help for test ignoring
 TESTS_TO_IGNORE=()
-UPDATE=0
-# Binarka:
-BINARY="./main.out" 
-LANG="C"
+UPDATE=""
+BINARY="./main.out" #default binary path
+#LANG="C"
 QUIET=""
-
+MAKE=""
+#COMPILATION=""
 quitable(){
 if [ ! "$QUIET" == "yes" ]; then
     printf "$*"
@@ -35,22 +35,13 @@ fi
 
 }
 compile(){
-
-    case $LANG in
-        C) compile_c;;
-    esac
-
-}
-compile_c(){
-# Progtest kompilace:
-gcc -g $SOURCE_FILES -o $BINARY -lm -std=c99  #2>/dev/null
-
-# Test kompilace:
-if [[ $? != 0 ]]; then
-    [[ $QUIET -eq  "yes" ]] && printf "There was an error during compilation..."
-	#echo "There was an error during compilation..."
-	exit 1
+if [ "$MAKE" == "yes" ]; then
+	COMPILATION="m" 
 fi
+
+    case $COMPILATION in
+        m) make;;
+    esac
 
 }
 
@@ -68,11 +59,8 @@ if [ $LOCAL = $REMOTE ]; then
     true
 elif [ $LOCAL = $BASE ]; then
     printf "There is new version available. Do you want to pull changes? (y/n) " | colorize green
-    #read -p CONSENT
     read -p "" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] && git pull
     	
-    	#(echo "$TEST_FILE" | grep -F -E "$IGNORE_REGEX" 1>/dev/null) && continue
-    #fi
 elif [ $REMOTE = $BASE ]; then
     true
     echo "Need to push"
@@ -83,61 +71,65 @@ fi
 show_help(){
 
     printf "Usage: ./test.sh ([OPTION] [ARGS]?)* 
-       Exit code is number of failed tests.
+       This script looks for all <test_file>.in, pipes them into your binary and then compares them with <test_file>.out.
+       Exit code is number of failed tests. 
       -h,            prints help
       -q,            supresses any text outputs
-      -s,            specify source files (main.c is default)
-      -L,            choose language (just \"C\" for now)
-      -u,            look for updates 
-      -d,            prints also difference in your_output and datapub_output
-      -i <TESTS>,    ignore certain tests, where <TESTS> are relative paths in datapub directory (\"test01.in test02.in\")
-      -iR <TESTS_R>, ignore tests in datapub directory with extended regex <TESTS_R> (like \"test1.\.in\" or \"test0[1-5].\.in\")
-      -r <REPEAT>,   repeat tests (useful for not innitialized variables errors)
-      -t <TIMEOUT>,  set timeout for tests\n"
+      -o,            target binary
+      -m,            compile with your local Makefile
+      -u,            look for update on this script at github
+      -d,            prints also the difference in your_output and datapub_output
+      -i <TESTS>,    ignore certain tests, where <TESTS> are relative paths in 'datapub' directory, like: (\"test01.in test02.in\")
+      -iR <TESTS_R>, ignore tests in 'datapub' directory with extended regex (grep -w -E <TESTS_R>)  (like \"test1.in\" or \"test0[1-5].*\")
+      -r <REPEAT>,   repeat selected tests for <REPEAT> times
+      -t <TIMEOUT>,  set timeout for the tests\n"
 }
 test_outputs(){
 
-# Pro vsechny soubory s nazvem "*.in" ve složce datapub...
 for TEST_FILE in ./datapub/*.in; do
 
 	if [ "$IGNORE_TESTS" == "yes" ]; then
-		#špatn 
+
 		if [ ! "$IGNORE_REGEX" == "" ]; then
-			(echo "$TEST_FILE" | grep -F -E "$IGNORE_REGEX" 1>/dev/null) && continue
+			(echo "$TEST_FILE" | grep -E "$IGNORE_REGEX" 1>/dev/null) && continue
 		fi
-		(echo "${TESTS_TO_IGNORE[*]}"  | grep -F -q "$TEST_FILE") && continue
+		(echo "${TESTS_TO_IGNORE[*]}"  | grep -w -q "$TEST_FILE") && continue
 		
 	fi
 	quitable ">>> Testing $TEST_FILE "
- 
-	DIFF=$(timeout "$TIMEOUT" diff "${TEST_FILE/in/out}" <($BINARY 2>/dev/null < $TEST_FILE ) ||  echo "TIMED OUT after $TIMEOUT")
+
+    DIFF=$(timeout "$TIMEOUT" diff "${TEST_FILE/in/out}" <($BINARY 2>/dev/null < $TEST_FILE ) ||  echo "TIMED OUT after $TIMEOUT")
+	
 	if [ "$DIFF" == "" ]; then
 		quitable "~ OK\n" | colorize green
 	else
 		quitable "~ FAILED\n" | colorize red
 		((RETURN++))
  		if [ "$DO_DIFF" == "yes" ]; then
-		# Vypis rozdil mezi vystupem programu a vzorovym vystupem
+
 			quitable "Output diff: %s\n" "$DIFF"
 		fi
 	fi
 done
 
-# Smaz binarku:
-
-
 }
 
-#if no args were passed just do plain run
 main(){
+set -e
+ls | grep -E -w "${BINARY##"./"}" &>/dev/null || (quitable "File \'${BINARY##"./"}\' doesnt exist\n" | colorize red; exit 1;)
+
 compile
 for i in $(seq $REPEAT);
 do 
 	quitable "\nRunning $i...\n"
 	test_outputs
 done
-tput bel
+
+if [ ! "$QUIET" == "yes" ]; then
+    tput bel
+fi
 }
+
 check_for_dependencies
 (
   # this flag will make to exit from current subshell on any error inside check_for_updates
@@ -156,24 +148,26 @@ done
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -h) show_help; exit 0;;
-        -q) QUIET="yes";;
+        -h) show_help; exit 0;;#
+        -q) QUIET="yes";;#
+        -o) BINARY="$2";shift;;#
         -s) SOURCE_FILES="$2"; quitable "Using $SOURCE_FILES source files...\n";shift;;
         -L) LANG="$2";quitable "Language $LANG...\n";shift;;
-        -u) UPDATE="yes";shift;;
-        -d) DO_DIFF="yes";;
-        -t) TIMEOUT="$2"; quitable "Running with timeout = $TIMEOUT\n";shift;;
-        -r) REPEAT="$2"; quitable "Running with repeat = $REPEAT\n";shift;;
-        -i) TESTS_TO_IGNORE+=("$2"); IGNORE_TESTS="yes"; quitable "Ignoring... = '%s'\n" "${TESTS_TO_IGNORE[*]}";shift;;
-        -iR) IGNORE_REGEX="$2"; IGNORE_TESTS="yes"; quitable "Ignoring... = '$IGNORE_REGEX'\n";shift;;
+        -m) MAKE="yes";;#
+        -u) UPDATE="yes";;#
+        -d) DO_DIFF="yes";;#
+        -t) TIMEOUT="$2"; quitable "Running with timeout = $TIMEOUT\n";shift;;#
+        -r) REPEAT="$2"; quitable "Running with repeat = $REPEAT\n";shift;;#
+        -i) TESTS_TO_IGNORE+=("$2"); IGNORE_TESTS="yes"; quitable "Ignoring... = '%s'\n" "${TESTS_TO_IGNORE[*]}";shift;;#
+        -iR) IGNORE_REGEX="$2"; IGNORE_TESTS="yes"; quitable "Ignoring... = '$IGNORE_REGEX'\n";shift;;#
         
         *) show_help; exit 1 ;;
     esac
     shift
 done
-
-main
-rm -f "$BINARY"
+(
+    main
+)
 
 exit $RETURN
 
